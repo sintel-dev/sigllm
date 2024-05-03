dcc4cb1dfadba29a79dfa9a8d6e06a6f68e1a31d
linhnk@mit.edu <klinh152001@gmail.com>
2024-05-03 16:01:44 -0400

    prompter pipeline

--- sigllm/primitives/prompting/anomalies.py
+++ sigllm/primitives/prompting/anomalies.py
@@ -1,107 +1,91 @@
 # -*- coding: utf-8 -*-
 
 """
-Result post-processing module.
+Prompter post-processing module
 
-This module contains functions that help convert model responses back to indices and timestamps.
+This module contains functions that help filter LLMs results to get the final anomalies.
 """
+
 import numpy as np
 
-
-def str2sig(text, sep=',', decimal=0):
-    """Convert a text string to a signal.
-
-    Convert a string containing digits into an array of numbers.
-
-    Args:
-        text (str):
-            A string containing signal values.
-        sep (str):
-            String that was used to separate each element in text, Default to `","`.
-        decimal (int):
-            Number of decimal points to shift each element in text to. Default to `0`.
-
-    Returns:
-        numpy.ndarray:
-            A 1-dimensional array containing parsed elements in `text`.
+def val2idx(vals, windows): 
+    """Convert detected anomalies values into indices.
+    
+    Convert windows of detected anomalies values into an array of all indices
+    in the input sequence that have those values. 
+    
+    Args: 
+        vals (List[ndarray]]): 
+            A list nd array containing detected anomalous values from different
+            responses of one window in one sample response. 
+        windows (ndarray):
+            rolling window sequences.      
+    Returns: 
+        List([ndarray]):
+            A list of nd array containing detected anomalous indices from different
+            responses of one window in one sample response.
     """
-    # Remove all characters from text except the digits and sep and decimal point
-    text = ''.join(i for i in text if (i.isdigit() or i == sep or i == '.'))
-    values = np.fromstring(text, dtype=float, sep=sep)
-    return values * 10**(-decimal)
 
+    idx_list = []
+    for anomalies_list, seq in zip(vals, windows): 
+        idx_win_list = []
+        for anomalies in anomalies_list:
+            mask = np.isin(seq, anomalies)
+            indices = np.where(mask)[0]
+            idx_win_list.append(indices)
+        idx_win_list = np.array(idx_win_list)
+        idx_list.append(idx_win_list)
+    return idx_list
 
-def str2idx(text, len_seq, sep=','):
-    """Convert a text string to indices.
-
-    Convert a string containing digits into an array of indices.
-
-    Args:
-        text (str):
-            A string containing indices values.
-        len_seq (int):
-            The length of processed sequence
-        sep (str):
-            String that was used to separate each element in text, Default to `","`.
-
-    Returns:
-        numpy.ndarray:
-            A 1-dimensional array containing parsed elements in `text`.
-    """
-    # Remove all characters from text except the digits and sep
-    text = ''.join(i for i in text if (i.isdigit() or i == sep))
-
-    values = np.fromstring(text, dtype=int, sep=sep)
-
-    # Remove indices that exceed the length of sequence
-    values = values[values < len_seq]
-    return values
-
-
-def get_anomaly_list_within_seq(res_list, alpha=0.5):
+def ano_within_windows(idx_win_list, alpha=0.5):
     """Get the final list of anomalous indices of a sequence
 
     Choose anomalous index in the sequence based on multiple LLM responses
 
     Args:
-        res_list (List[numpy.ndarray]):
-            A list of 1-dimensional array containing anomous indices output by LLM
+        idx_win_list (List[List[numpy.ndarray]]):
+            A list of lists of 1d array containing detected anomalous indices of
+            one window in one sample response.
         alpha (float):
-            Percentage of votes needed for an index to be deemed anomalous. Default: 0.5
+            Percentage of votes needed for an index to be deemed anomalous. Default to `0.5`.
 
     Returns:
-        numpy.ndarray:
-            A 1-dimensional array containing final anomalous indices
+        List[numpy.ndarray]:
+            A list of 1-dimensional array containing final anomalous indices of each windows.
     """
-    min_vote = np.ceil(alpha * len(res_list))
+    
+    idx_list = []
+    for samples in idx_win_list:
+        min_vote = np.ceil(alpha * len(samples))
 
-    flattened_res = np.concatenate(res_list)
+        flattened_res = np.flatten(samples)
 
-    unique_elements, counts = np.unique(flattened_res, return_counts=True)
+        unique_elements, counts = np.unique(flattened_res, return_counts=True)
 
-    final_list = unique_elements[counts >= min_vote]
+        final_list = unique_elements[counts >= min_vote]
 
-    return final_list
+        idx_list.append(final_list)
 
+    return idx_list
 
 def merge_anomaly_seq(anomalies, start_indices, window_size, step_size, beta=0.5):
     """Get the final list of anomalous indices of a sequence when merging all rolling windows
 
     Args:
         anomalies (List[numpy.ndarray]):
-            A list of 1-dimensional array containing anomous indices of each window
+            A list of 1-dimensional array containing anomous indices of each window.
         start_indices (numpy.ndarray):
-            A 1-dimensional array contaning the first index of each window
+            A 1-dimensional array contaning the first index of each window.
         window_size (int):
-            Length of each window
+            Length of each window.
         step_size (int):
             Indicating the number of steps the window moves forward each round.
         beta (float):
-            Percentage of containing windows needed for index to be deemed anomalous. Default: 0.5
+            Percentage of containing windows needed for index to be deemed anomalous. Default to `0.5`.
 
     Return:
         numpy.ndarray:
-            A 1-dimensional array containing final anomalous indices
+            A 1-dimensional array containing final anomalous indices.
     """
     anomalies = [arr + first_idx for (arr, first_idx) in zip(anomalies, start_indices)]
 
@@ -115,18 +99,55 @@ def merge_anomaly_seq(anomalies, start_indices, window_size, step_size, beta=0.5
 
     return np.sort(final_list)
 
-
 def idx2time(sequence, idx_list):
     """Convert list of indices into list of timestamp
 
     Args:
         sequence (pandas.Dataframe):
-            Signal with timestamps and values
+            Signal with timestamps and values.
         idx_list (numpy.ndarray):
-            A 1-dimensional array of indices
+            A 1-dimensional array of indices.
 
     Returns:
         numpy.ndarray:
-            A 1-dimensional array containing timestamps
+            A 1-dimensional array containing timestamps.
     """
     return sequence.iloc[idx_list].timestamp.to_numpy()
+
+def timestamp2interval(timestamp_list, interval, start, end, padding_size = 50): 
+    """Convert list of timestamps to list of intervals by padding to both sides
+    and merge overlapping 
+    
+    Args: 
+        timestamp_list (List[timestamp]): 
+            A list of point timestamps.
+        interval (int):
+            The fixed gap between two consecutive timestamps of the time series.
+        start (timestamp): 
+            The start timestamp of the time series.
+        end (timestamp): 
+            The end timestamp of the time series.
+        padding_size (int): 
+            Number of steps to pad on both sides of a timestamp point.
+    """
+    intervals = []
+    for timestamp in timestamp_list: 
+        intervals.append((max(start, timestamp-padding_size*interval), min(end, timestamp+padding_size*interval)))
+
+    if not intervals:
+        return []
+
+    intervals.sort(key=lambda x: x[0])  # Sort intervals based on start time
+    merged_intervals = [intervals[0]]  # Initialize merged intervals with the first interval
+
+    for current_interval in intervals[1:]:
+        previous_interval = merged_intervals[-1]
+        
+        # If the current interval overlaps with the previous one, merge them
+        if current_interval[0] <= previous_interval[1]:
+            previous_interval = (previous_interval[0], max(previous_interval[1], current_interval[1]))
+            merged_intervals[-1] = previous_interval
+        else:
+            merged_intervals.append(current_interval)  # Append the current interval if no overlap
+
+    return merged_intervals
--- sigllm/primitives/prompting/gpt.py
+++ sigllm/primitives/prompting/gpt.py
@@ -1,10 +1,130 @@
 # -*- coding: utf-8 -*-
 
-"""
-GPT model module.
+import json
+import os
 
-This module contains functions that are specifically used for GPT models
-"""
+import openai
+import tiktoken
+
+PROMPT_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'gpt_messages.json'
+)
+
+PROMPTS = json.load(open(PROMPT_PATH))
+
+VALID_NUMBERS = list("0123456789 ")
+BIAS = 30
+
+
+class GPT:
+    """Prompt GPT models to detect anomalies in a time series.
+
+    Args:
+        name (str):
+            Model name. Default to `'gpt-3.5-turbo'`.
+        sep (str):
+            String to separate each element in values. Default to `','`.
+    """
+
+    def __init__(self, name='gpt-3.5-turbo', sep=','):
+        self.name = name
+        self.sep = sep
+
+        self.tokenizer = tiktoken.encoding_for_model(self.name)
+
+        valid_tokens = []
+        for number in VALID_NUMBERS:
+            token = self.tokenizer.encode(number)
+            valid_tokens.append(token)
+
+        valid_tokens.append(self.tokenizer.encode(self.sep))
+        self.logit_bias = {token: BIAS for token in valid_tokens}
+
+    def detect(self, text, anomalous_percent = 0.5, temp=1, top_p=1, logprobs=False, top_logprobs=None,
+                 samples=10, seed=None):
+        """Use GPT to forecast a signal.
+
+        Args:
+            text (str):
+                A string containing signal values.
+            anomalous_percent (float): 
+                Expected percentage of time series that are anomalous. Default to `0.5`.
+            temp (float):
+                Sampling temperature to use, between 0 and 2. Higher values like 0.8 will
+                make the output more random, while lower values like 0.2 will make it
+                more focused and deterministic. Do not use with `top_p`. Default to `1`.
+            top_p (float):
+                Alternative to sampling with temperature, called nucleus sampling, where the
+                model considers the results of the tokens with top_p probability mass.
+                So 0.1 means only the tokens comprising the top 10% probability mass are
+                considered. Do not use with `temp`. Default to `1`.
+            logprobs (bool):
+                Whether to return the log probabilities of the output tokens or not.
+                Defaults to `False`.
+            top_logprobs (int):
+                An integer between 0 and 20 specifying the number of most likely tokens
+                to return at each token position. Default to `None`.
+            samples (int):
+                Number of responses to generate for each input message. Default to `10`.
+            seed (int):
+                Beta feature by OpenAI to sample deterministically. Default to `None`.
+
+        Returns:
+            list, list:
+                * List of detected anomalous values.
+                * Optionally, a list of the output tokens' log probabilities.
+        """
+        input_length = len(self.tokenizer.encode(text))
+        max_tokens = input_length * anomalous_percent
+
+        message = ' '.join(PROMPTS['user_message'], text, self.sep)
+        response = openai.ChatCompletion.create(
+            model=self.name,
+            messages=[
+                {"role": "system", "content": PROMPTS['system_message']},
+                {"role": "user", "content": message}
+            ],
+            max_tokens=max_tokens,
+            temperature=temp,
+            logprobs=logprobs,
+            top_logprobs=top_logprobs,
+            n=samples,
+        )
+        responses = [choice.message.content for choice in response.choices]
+        if logprobs:
+            probs = [choice.logprobs for choice in response.choices]
+            return responses, probs
+
+        return responses
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 
 from openai import OpenAI
